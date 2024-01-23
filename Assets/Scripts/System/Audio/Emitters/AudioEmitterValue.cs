cd49315efc60f90d8b16acd21d14dbada710aa95
Thành Long (Morningstar WiIIiam) <us.thanhlong18@gmail.com>
2024-01-23 22:43:30 +0700

    feat(background-music): add emmitter and config to play (#25)

---
+++ Assets/Scripts/System/Audio/Emitters/AudioEmitterValue.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine.Events;
+
+namespace Long18.System.Audio.Emitters
+{
+    [Serializable]
+    public struct AudioEmitterValue
+    {
+        private AudioEmitter _audioEmitter;
+        public AudioEmitterValue(AudioEmitter audioEmitter) => _audioEmitter = audioEmitter;
+
+        public void UnregisterAudioFinishedPlayingEvent(UnityAction<AudioEmitterValue> audioFinishedPlaying)
+        {
+            if (_audioEmitter) _audioEmitter.OnFinishedPlaying -= audioFinishedPlaying;
+        }
+
+        public void StopAudio()
+        {
+            if (_audioEmitter) _audioEmitter.Stop();
+        }
+    }
+}
